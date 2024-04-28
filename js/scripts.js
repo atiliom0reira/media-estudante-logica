433652ded9ab2a5fbaf3ce8e7c1aba6054abe910
Kari Atílio Moreira <202401635278@alunos.estacio.br>
2024-04-27 22:02:55 -0300

    feat: adicionando mais uma condição na média

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,10 @@ average = average.toFixed(2)
 
 if(result) {
     alert("Parabéns, " + student + "! Sua média foi de: " + average)
-}  else {
+} 
+else if (average < 3) {
+    alert("Reprovado")
+}  
+else {
  alert(student + ", estude para sua prova de recuperação! Sua média foi de: " + average)
 }
